2e6dbf354c542f15fe8dee974c67f4e04f8e3ede
kolbas86 <kolbas86@gmail.com>
2017-12-03 23:25:03 +0300

    edited comments

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -6,14 +6,17 @@ import java.util.Scanner;
 
 public class Base {
     public static void main(String[] args) {
+                                                // Create Scanner
         Scanner Base = new Scanner(System.in);
         double fn, sn, result;
         System.out.println("Введи первое число");
+                                                // first number
         fn = Base.nextDouble();
         System.out.println("Введи второе число");
+                                               // second number
         sn = Base.nextDouble();
         result = fn + sn;
-        System.out.println("Сумма равна " + result);
+        System.out.printf("Сумма равна %.4f " ,result);
 
         Base.close();
     }
